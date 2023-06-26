56f8a1c16bdc1410ad843d34d57295739bc27d8d
KhizerZakir <115882297+khizerzakir@users.noreply.github.com>
2023-06-26 11:11:50 +0200

    Add files via upload

---
+++ A4_Zakir/geofunction_module/__init__.py
@@ -0,0 +1,30 @@
+import os
+import glob
+import matplotlib.pyplot as plt
+import xarray as xr
+import rasterio
+from rasterio.plot import show
+import numpy as np
+import pandas as pd
+
+
+def read_and_plot_tiff(file_path):
+    # Function code...
+    
+def plot_raster_with_coordinates(path_tiff):
+    # Function code...
+
+def calculate_ndvi(path_tiff):
+    # Function code...
+
+def plot_ndvi(ndvi):
+    # Function code...
+
+def print_ndvi_stats(ndvi):
+    # Function code...
+
+def get_top_ndvi_values(ndvi, num_values=10):
+    # Function code...
+
+def draw_square_on_raster(path_tiff):
+    # Function code...
---
+++ A4_Zakir/geofunction_module/test.py
@@ -0,0 +1,6 @@
+import geofunctions_A4_Khizer as g4
+file_path = "E:/A4"
+path_tiff = "E:/A4/QuickBird.tif"
+g4.read_and_plot_tiff(file_path)
+
+
